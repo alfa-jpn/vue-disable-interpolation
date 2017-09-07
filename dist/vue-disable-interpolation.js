9fd438bd479329481cf57c2a47dbb6b3bd15a174
alfa <alfa.jpn@gmail.com>
2017-09-07 12:34:27 +0900

    Build library

---
+++ dist/vue-disable-interpolation.js
@@ -0,0 +1,77 @@
+var VueDisableInterpolation = function(modules) {
+    function __webpack_require__(moduleId) {
+        if (installedModules[moduleId]) return installedModules[moduleId].exports;
+        var module = installedModules[moduleId] = {
+            i: moduleId,
+            l: !1,
+            exports: {}
+        };
+        return modules[moduleId].call(module.exports, module, module.exports, __webpack_require__), 
+        module.l = !0, module.exports;
+    }
+    var installedModules = {};
+    return __webpack_require__.m = modules, __webpack_require__.c = installedModules, 
+    __webpack_require__.d = function(exports, name, getter) {
+        __webpack_require__.o(exports, name) || Object.defineProperty(exports, name, {
+            configurable: !1,
+            enumerable: !0,
+            get: getter
+        });
+    }, __webpack_require__.n = function(module) {
+        var getter = module && module.__esModule ? function() {
+            return module.default;
+        } : function() {
+            return module;
+        };
+        return __webpack_require__.d(getter, "a", getter), getter;
+    }, __webpack_require__.o = function(object, property) {
+        return Object.prototype.hasOwnProperty.call(object, property);
+    }, __webpack_require__.p = "", __webpack_require__(__webpack_require__.s = 0);
+}([ function(module, exports, __webpack_require__) {
+    "use strict";
+    var _vue_disable_interpolation = __webpack_require__(1), _vue_disable_interpolation2 = function(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }(_vue_disable_interpolation);
+    module.exports = _vue_disable_interpolation2.default;
+}, function(module, exports, __webpack_require__) {
+    "use strict";
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) throw new TypeError("Cannot call a class as a function");
+    }
+    Object.defineProperty(exports, "__esModule", {
+        value: !0
+    });
+    var _createClass = function() {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || !1, descriptor.configurable = !0, 
+                "value" in descriptor && (descriptor.writable = !0), Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
+        return function(Constructor, protoProps, staticProps) {
+            return protoProps && defineProperties(Constructor.prototype, protoProps), staticProps && defineProperties(Constructor, staticProps), 
+            Constructor;
+        };
+    }(), _class = function() {
+        function _class() {
+            _classCallCheck(this, _class);
+        }
+        return _createClass(_class, null, [ {
+            key: "install",
+            value: function(Vue) {
+                var DELIMITER_PATCH = {
+                    replace: function() {
+                        return "^(?!.).";
+                    }
+                };
+                Vue.mixin({
+                    delimiters: [ DELIMITER_PATCH, DELIMITER_PATCH ]
+                });
+            }
+        } ]), _class;
+    }();
+    exports.default = _class;
+} ]);
